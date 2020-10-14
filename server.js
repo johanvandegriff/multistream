2424dcf45860c3fe89329132c8882261e14a4d3a
Johan Vandegriff <johan@vandymail.com>
2020-10-14 09:17:10 -0400

    dynamic CSS layout for chat

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {res.sendFile(__dirname + '/index.html')});
-app.get('/check.txt', (req, res) => {res.send('success');});
 app.get('/jquery.js', (req, res) => {res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.js')});
 app.get('/color-hash.js', (req, res) => {res.sendFile(__dirname + '/node_modules/color-hash/dist/color-hash.js')});
 app.get('/video.js', (req, res) => {res.sendFile(__dirname + '/node_modules/video.js/dist/video.js')});
 app.get('/video-js.css', (req, res) => {res.sendFile(__dirname + '/node_modules/video.js/dist/video-js.css')});
-app.get('/stream.png', (req, res) => {res.sendFile(__dirname + '/stream.png')});
 
+app.use('/static', express.static('static'));
 app.use('/live', express.static('live'));
 
 io.on('connection', (socket) => {
@@ -22,6 +21,11 @@ io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
         console.log('message: ' + msg.name + ": " + msg.text);
         io.emit('chat message', msg);
+        //emit the message many times for testing CSS
+        // msg.text = msg.text+msg.text+msg.text+msg.text+msg.text+msg.text+msg.text+msg.text+msg.text+msg.text+msg.text+msg.text+msg.text+msg.text;
+        // for (i=0; i<30; i++) {
+        //     io.emit('chat message', msg);
+        // }
     });
 });
 
@@ -30,10 +34,11 @@ http.listen(process.env.PORT || default_port, () => {
     console.log('listening on *:' + (process.env.PORT || default_port));
 });
 
-
-//TODO CSS dynamic layout: wide mode = layout fixed width chat on right. narrow = stream on top, chat on bottom
 //TODO clean up the CSS, make text look good
 //TODO add cards instead of links
 //TODO pull twitch chat
 //TODO only change nickname when email is provided
 //TODO save nickname with cookies
+//TODO add a README with setup instructions
+//TODO favicon
+//TODO use flexbox for CSS?
